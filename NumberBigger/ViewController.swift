2f670e452848e7687022a071fe7217389914e7eb
Makarerk Boonyaleephan <makarerk@gmail.com>
2017-10-14 20:25:07 +0700

    Initial Commit

---
+++ NumberBigger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberBigger
+//
+//  Created by Makarerk Boonyaleephan on 14/10/2560 BE.
+//  Copyright © 2560 Makarerk Boonyaleephan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
